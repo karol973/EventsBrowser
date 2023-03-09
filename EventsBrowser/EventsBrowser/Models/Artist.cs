b63d79ddee1c7b07426eddf667799112365d4247
pszybyszek <33810555+karol973@users.noreply.github.com>
2023-03-09 20:43:10 +0100

    Update Artist.cs

--- EventsBrowser/EventsBrowser/Models/Artist.cs
+++ EventsBrowser/EventsBrowser/Models/Artist.cs
@@ -8,25 +8,22 @@ namespace EventsBrowser.Models
 {
     public class Artist
     {
-        [Key]                             // primary key
-        public int Id { get; set; }       // entinty framework rozpoznaje samo "id"
+        [Key]                            
+        public int Id { get; set; }       
         [Display(Name ="Zdjęcie profilowe")]
         [Required(ErrorMessage = "Zdjęcie profilowe jest wymagane")]
         public string ProfilePictureUrl { get; set; }
 
-        //walidacja nazwy
         [Display(Name = "Nazwa")]
         [Required(ErrorMessage = "Nazwa jest wymagana")]
         [StringLength(50,MinimumLength =2, ErrorMessage = "Nazwa musi mieć co najmniej 2 znaki")]
         public string FullName { get; set; }
 
-        // walidacja Bio
         [Display(Name = "Bio")]
         [Required(ErrorMessage = "To pole jest wymagane")]
         [StringLength(50, MinimumLength = 5, ErrorMessage = "Bio musi zawierać minimum 5 znaków")]
         public string Bio { get; set; }
 
-        //Relationship
         public List<Artist_Event> Artists_Events { get; set; }
     }
 }
